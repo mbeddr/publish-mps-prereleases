cf79048a6de9eaa253e9b0476d73a086b8d6ce9e
Sergej Koščejev <sergej@koscejev.cz>
2024-01-17 13:38:37 +0100

    Remove unnecessary dependency

--- find-latest-version/build.gradle.kts
+++ find-latest-version/build.gradle.kts
@@ -14,21 +14,12 @@ repositories {
 dependencies {
     implementation("org.jetbrains.teamcity:teamcity-rest-client:1.21.0")
 
-    // teamcity-rest-client 1.21.0 erroneously omits transitive dependencies so we have to include them explicitly.
+    // teamcity-rest-client 1.21.0 erroneously omits transitive dependencies, so we have to include them explicitly.
     runtimeOnly("com.squareup.retrofit:retrofit:1.9.0")
     runtimeOnly("com.squareup.okhttp3:okhttp:4.7.2")
     runtimeOnly("com.jakewharton.retrofit:retrofit1-okhttp3-client:1.1.0")
 
     runtimeOnly("org.slf4j:slf4j-jdk14:1.7.+")
-
-    val mavenResolverVersion = "1.9.18"
-
-    implementation("org.apache.maven.resolver:maven-resolver-supplier")
-    implementation("org.apache.maven.resolver:maven-resolver-util")
-    implementation("org.apache.maven:maven-resolver-provider:3.9.6")
-
-    implementation(platform("org.apache.maven.resolver:maven-resolver:${mavenResolverVersion}"))
-
 }
 
 tasks.run.configure {
